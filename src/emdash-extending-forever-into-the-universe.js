86b6e2a2a1c7522649317f2e299b5ad69e264216
Mackenzie Davidson <mwvdavidson@gmail.com>
2019-04-16 08:22:19 -0400

    no longer needs underscore.js

--- src/emdash-extending-forever-into-the-universe.js
+++ src/emdash-extending-forever-into-the-universe.js
@@ -1,27 +1,22 @@
-function setDefaults(options, defaults){
-    return _.defaults({}, _.clone(options), defaults);
-}
-
 function foreverIntoTheUniverse (options) {
-    var defaults = {
-        howLong: 2,
-        whatToReplace: '—',
-        whereToReplace: 'body',
-        howMuchCloserCouldWePossiblyBe: '0.25'
-    };
-    options = setDefaults(options, defaults);
-    var extend = "<span style=\"letter-spacing:-" + options.howMuchCloserCouldWePossiblyBe + "em\">" + options.whatToReplace + "</span>"
-    var replaceMe = options.whereToReplace.slice(0,1)
-    var withMe = options.whereToReplace.substring(1)
-    if (options.whereToReplace === 'body') {
-        document.body.innerHTML = document.body.innerHTML.replace(new RegExp(options.whatToReplace, 'g'), extend.repeat(options.howLong-1) + options.whatToReplace);
-    } else if (options.whereToReplace.slice(0,1) === '#') {
+
+    if (typeof(howLong)==='undefined') howLong = 2;
+    if (typeof(whatToReplace)==='undefined') whatToReplace = '—';
+    if (typeof(whereToReplace)==='undefined') whatToReplace = 'body';
+    if (typeof(howMuchCloserCouldWePossiblyBe)==='undefined') whatToReplace = '0.25';
+
+    var extend = "<span style=\"letter-spacing:-" + howMuchCloserCouldWePossiblyBe + "em\">" + whatToReplace + "</span>"
+    var replaceMe = whereToReplace.slice(0,1)
+    var withMe = whereToReplace.substring(1)
+    if (whereToReplace === 'body') {
+        document.body.innerHTML = document.body.innerHTML.replace(new RegExp(whatToReplace, 'g'), extend.repeat(howLong-1) + whatToReplace);
+    } else if (whereToReplace.slice(0,1) === '#') {
         var elementId = document.getElementById(withMe).innerHTML
-        document.getElementById(withMe).innerHTML = elementId.replace(new RegExp(options.whatToReplace, 'g'), extend.repeat(options.howLong-1) + options.whatToReplace);
-    } else if (options.whereToReplace.slice(0,1) === '.') {
+        document.getElementById(withMe).innerHTML = elementId.replace(new RegExp(whatToReplace, 'g'), extend.repeat(howLong-1) + whatToReplace);
+    } else if (whereToReplace.slice(0,1) === '.') {
         var elementClass = document.getElementsByClassName(withMe)
         for (var i=0;i<elementClass.length;i++){
-            document.getElementsByClassName(withMe)[i].innerHTML = document.getElementsByClassName(withMe)[i].innerHTML.replace(new RegExp(options.whatToReplace, 'g'), extend.repeat(options.howLong-1) + options.whatToReplace);
+            document.getElementsByClassName(withMe)[i].innerHTML = document.getElementsByClassName(withMe)[i].innerHTML.replace(new RegExp(whatToReplace, 'g'), extend.repeat(howLong-1) + whatToReplace);
         }
     } else {
         console.log('idk bro this shit is broken 4 u i guess')
