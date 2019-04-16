20a4bd9f79c0f69d9fb669cfeae3ce78605d6832
Mackenzie Davidson <mwvdavidson@gmail.com>
2019-04-16 06:56:49 -0400

    decided to write good js i guess lol

--- src/emdash-extending-forever-into-the-universe.js
+++ src/emdash-extending-forever-into-the-universe.js
@@ -6,7 +6,7 @@ function foreverIntoTheUniverse (options) {
     var defaults = {
         howLong: 2,
         whatToReplace: '—',
-        whereToReplace: "body",
+        whereToReplace: 'body',
         howMuchCloserCouldWePossiblyBe: '0.25'
     };
     options = setDefaults(options, defaults);
@@ -14,16 +14,16 @@ function foreverIntoTheUniverse (options) {
     var replaceMe = options.whereToReplace.slice(0,1)
     var withMe = options.whereToReplace.substring(1)
     if (options.whereToReplace === 'body') {
-        document.body.innerHTML = document.body.innerHTML.replace(new RegExp(options.whatToReplace, "g"), extend.repeat(options.howLong-1) + options.whatToReplace);
+        document.body.innerHTML = document.body.innerHTML.replace(new RegExp(options.whatToReplace, 'g'), extend.repeat(options.howLong-1) + options.whatToReplace);
     } else if (options.whereToReplace.slice(0,1) === '#') {
         var elementId = document.getElementById(withMe).innerHTML
-        document.getElementById(withMe).innerHTML = elementId.replace(new RegExp(options.whatToReplace, "g"), extend.repeat(options.howLong-1) + options.whatToReplace);
+        document.getElementById(withMe).innerHTML = elementId.replace(new RegExp(options.whatToReplace, 'g'), extend.repeat(options.howLong-1) + options.whatToReplace);
     } else if (options.whereToReplace.slice(0,1) === '.') {
         var elementClass = document.getElementsByClassName(withMe)
         for (var i=0;i<elementClass.length;i++){
-            document.getElementsByClassName(withMe)[i].innerHTML = document.getElementsByClassName(withMe)[i].innerHTML.replace(new RegExp(options.whatToReplace, "g"), extend.repeat(options.howLong-1) + options.whatToReplace);
+            document.getElementsByClassName(withMe)[i].innerHTML = document.getElementsByClassName(withMe)[i].innerHTML.replace(new RegExp(options.whatToReplace, 'g'), extend.repeat(options.howLong-1) + options.whatToReplace);
         }
     } else {
-        console.log("idk bro this shit is broken 4 u i guess")
+        console.log('idk bro this shit is broken 4 u i guess')
     }
 }
